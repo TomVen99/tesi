c96e3ebaf2c4667744ec66334385d191a1946fe5
Tomas Ventrucci <tomas.ventrucci@studio.unibo.it>
2024-08-08 16:45:35 +0200

    m

--- app/src/main/java/com/example/smartlagoon/ui/screens/ranking/RankingScreen.kt
+++ app/src/main/java/com/example/smartlagoon/ui/screens/ranking/RankingScreen.kt
@@ -1,7 +1,9 @@
 package com.example.smartlagoon.ui.screens.ranking
 
+import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -11,7 +13,10 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ListItem
@@ -21,8 +26,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.rememberAsyncImagePainter
+import com.example.smartlagoon.R
 import com.example.smartlagoon.data.database.User
 import com.example.smartlagoon.ui.composables.TopAppBar
 import com.example.smartlagoon.ui.viewmodel.UsersState
@@ -51,54 +60,34 @@ fun RankingScreen(
             contentPadding = PaddingValues(8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            stickyHeader {
-                Box(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.tertiaryContainer)
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .padding(top = 5.dp),
-                            //.border(1.dp, MaterialTheme.colorScheme.primaryContainer),
-                        verticalArrangement = Arrangement.Top
-                    ) {
-                        PrintRanking(
-                            state.users
+            itemsIndexed(state.users){ _, user ->
+                ListItem(
+                    headlineContent = { Text(text = user.username) },
+                    supportingContent = {
+                        Text(text = user.name)
+                    },
+                    leadingContent = {
+                        val painter =
+                            if (user.urlProfilePicture != "")
+                                rememberAsyncImagePainter(Uri.parse(user.urlProfilePicture))
+                            else {
+                                painterResource(id = R.drawable.ic_launcher_foreground)
+                            }
+                        Image(
+                            painter = painter,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(64.dp)
+                                .padding(4.dp),
+                            contentScale = ContentScale.Crop
                         )
-                    }
-                }
+                    },
+                    trailingContent = { Text(text = user.points.toString())},
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .border(1.dp, MaterialTheme.colorScheme.primaryContainer, RoundedCornerShape(16.dp)),
+                )
             }
         }
     }
 }
-
-private fun getUserListToPrint(user: User) : List<User> {
-    return listOf(user)
-}
-
-@Composable
-private fun PrintRanking(users: List<User> ) {
-    Log.d("classifica", "son qui")
-    for(user in users) {
-        /*Card(
-            modifier = Modifier
-                .fillMaxWidth(),
-            colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.tertiaryContainer
-            )
-        ) {*/
-            ListItem(
-                headlineContent = { Text(text = user.username) },
-                supportingContent = {
-                    Text(text = user.name)
-                },
-                trailingContent = { Text(text = user.points.toString())},
-                modifier = Modifier
-                    .padding(top = 5.dp)
-                    .border(1.dp, MaterialTheme.colorScheme.primaryContainer),
-            )
-        //}
-    }
-}
-
-
-
